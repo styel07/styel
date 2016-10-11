a04e2a28091b6f0f4c4d3ec7e4da22a89f3ea4b2
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-10-10 20:28:08 -0700

    removed default jade engine

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,16 +12,19 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use('/static', express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+app.use(logger());
 
 app.use('/', routes);
 app.use('/users', users);
@@ -50,11 +54,14 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.status(500).json({
+        message: err.message,
+        error: err
+    });
 });
 
+// app.listen(PORT, function() {
+// console.log('Listening on port' + PORT);
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(500).json({
+      message: err.message,
+      error: err
+  });
 });
 
 module.exports = router;
