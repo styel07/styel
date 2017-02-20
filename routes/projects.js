7b043b9fcdc1efc8de0e2da3d1e3f54e5b6ce185
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2017-02-20 13:39:05 -0800

    added put and delete methods to api

--- routes/projects.js
+++ routes/projects.js
@@ -6,8 +6,24 @@ router.get('/', (req, res, next) => {
   res.send('get project list');
 });
 
-router.post('/:name', (req, res, next) => {
+router.post('/add', (req, res, next) => {
   req.send('post project');
+});
+
+router.put('/update/:id', (req, res, next) => {
+  if(req.body.params.id) {
+    res.send('updated project');
+  } else {
+    res.send('unable to updated project');
+  }
+});
+
+router.delete('/delete/:id', (req, res, next) => {
+  if(req.body.params.id) {
+    res.send('deleted project');
+  } else {
+    res.send('unable to delete project');
+  }
 });
 
 
--- routes/skills.js
+++ routes/skills.js
@@ -6,8 +6,23 @@ router.get('/', (req, res, next) => {
   res.send('get all skills');
 });
 
-router.post('/add/:id', (req, res, next) => {
+router.post('/add', (req, res, next) => {
   res.send('add new skill');
 });
 
+router.put('/update/:id', (req, res, next) => {
+  if(req.body.params.id) {
+    res.send('updated skill');
+  } else {
+    res.send('unable to update skill');
+  }
+});
+
+router.put('/delete/:id', (req, res, next) => {
+  if(req.body.params.id) {
+    res.send('deleted skill');
+  } else {
+    res.send('unable to delete skill');
+  }
+});
 module.exports = router;
