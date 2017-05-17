8245c76b7cfa8bcba3068005b70fbd141d5ca8dd
eleazar corpuz <eleazar.corpuz@experianinteractive.com>
2017-05-17 14:16:58 -0700

    modified dash, home, skills ui

--- angular-src/src/app/components/skills/skills.component.ts
+++ angular-src/src/app/components/skills/skills.component.ts
@@ -8,7 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills: Object;
+  skills = [
+		{ name: 'angular', duration: 2, type: 'f' },
+		{ name: 'express', duration: 2, type: 'b' },
+		{ name: 'sql', duration: 2, type: 'b' },
+		{ name: 'javascript', duration: 4, type: 'f' },
+		{ name: 'sass', duration: 1, type: 'f' },
+		{ name: 'css', duration: 4, type: 'f' },
+		{ name: 'html', duration: 4, type: 'f' },
+		{ name: 'react', duration: 1, type: 'f' },
+		{ name: 'webpack', duration: 1, type: 'f'},
+		{ name: 'gulp', duration: 1, type: 'b'}
+	];
 
   constructor(
     private skillService: SkillService,
